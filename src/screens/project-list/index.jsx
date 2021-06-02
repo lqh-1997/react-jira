c8ee73173f00130eda8debcee4f7de6994a302c6
lqh-1997 <515514053@qq.com>
2021-06-02 16:17:37 +0800

    feat: 添加custom hook useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,49 +1,43 @@
-import { useEffect, useState } from "react";
-import { SearchPanel } from "./search-panel";
-import { List } from "./list";
-import { cleanObject } from "utils";
-import qs from "qs";
+import { useEffect, useState } from 'react';
+import { SearchPanel } from './search-panel';
+import { List } from './list';
+import { cleanObject, useDebounce, useMount } from 'utils';
+import qs from 'qs';
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export const ProjectListScreen = () => {
   // 这个是当前选择的人物信息
   const [param, setParam] = useState({
-    name: "",
-    personId: "",
+    name: '',
+    personId: '',
   });
+  const debouncedParam = useDebounce(param, 200);
 
   // 这个是可选择的用户列表
   // eslint-disable-next-line no-unused-vars
   const [users, setUsers] = useState([]);
-
   const [list, setList] = useState([]);
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
-      async (res) => {
-        if (res.ok) {
-          setList(await res.json());
-        }
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async (res) => {
+      if (res.ok) {
+        setList(await res.json());
       }
-    );
-  }, [param]);
+    });
+  }, [debouncedParam]);
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (res) => {
       if (res.ok) {
         setUsers(await res.json());
       }
     });
-  }, []);
+  });
 
   return (
     <div>
-      <SearchPanel
-        param={param}
-        setParam={setParam}
-        users={users}
-      ></SearchPanel>
+      <SearchPanel param={param} setParam={setParam} users={users}></SearchPanel>
       <List list={list} users={users}></List>
     </div>
   );
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 /**
  * 是否为假值 null undefined '' NaN 0n false
  * @param {*} value
@@ -18,4 +20,24 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebounceValue(value), delay);
+
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debounceValue;
 };
