aceb5eb80e2d1413375ffd4ce4df4c3e58803a8b
lqh-1997 <515514053@qq.com>
2021-06-07 17:06:28 +0800

    style: 优化项目页面

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -3,6 +3,7 @@ import { SearchPanel } from './search-panel';
 import { List } from './list';
 import { cleanObject, useDebounce, useMount } from 'utils';
 import { useHttp } from 'utils/http';
+import styled from '@emotion/styled';
 
 export const ProjectListScreen = () => {
   // 这个是当前选择的人物信息
@@ -26,9 +27,14 @@ export const ProjectListScreen = () => {
   });
 
   return (
-    <div>
+    <Container>
+      <h1>项目列表</h1>
       <SearchPanel param={param} setParam={setParam} users={users}></SearchPanel>
       <List list={list} users={users}></List>
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  padding: 3.2rem;
+`;
--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,4 +1,6 @@
+// import { jsx } from '@emotion/react';
 import { Input, Select } from 'antd';
+import { Form } from 'antd';
 
 export interface User {
   id: string;
@@ -20,10 +22,12 @@ interface SearchPanelProps {
 
 export const SearchPanel = ({ param, setParam, users }: SearchPanelProps) => {
   return (
-    <form>
-      <div>
+    // 要使用emotion行内样式 需要在头部加上注释 和 引入
+    <Form layout={'inline'} style={{ marginBottom: '2rem' }}>
+      <Form.Item>
         {/* 输入框 */}
         <Input
+          placeholder="项目名"
           type="text"
           value={param.name}
           onChange={(evt) =>
@@ -33,6 +37,8 @@ export const SearchPanel = ({ param, setParam, users }: SearchPanelProps) => {
             })
           }
         />
+      </Form.Item>
+      <Form.Item>
         {/* 下拉选择框 */}
         <Select
           value={param.personId}
@@ -52,7 +58,7 @@ export const SearchPanel = ({ param, setParam, users }: SearchPanelProps) => {
             );
           })}
         </Select>
-      </div>
-    </form>
+      </Form.Item>
+    </Form>
   );
 };
